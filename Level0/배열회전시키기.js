50581cc3c4c2dde2e6fcae2748e786b318c1e399
0taepark <ytpark1665@gmail.com>
2022-11-24 16:59:03 +0900

    Level0 배열회전시키기:수정

--- Level0/배열회전시키기.js
+++ Level0/배열회전시키기.js
@@ -1,11 +1,6 @@
 function solution(numbers, direction) {
-if(direction === "right") {
-   numbers.unshift(numbers.pop())
-}
-else {
-     numbers.push(numbers.shift())
-}
-return numbers
+    direction === "right" ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift())
+    return numbers
 }
 
-console.log(solution([1, 2, 3],"right"))
+console.log(solution([1, 2, 3, 4, 5],"right"))

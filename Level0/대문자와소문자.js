35c388e2330cdad483d22e8981ac371add376417
0taepark <ytpark1665@gmail.com>
2022-11-29 13:51:27 +0900

    Level0 대문자와소문자 수정

--- Level0/대문자와소문자.js
+++ Level0/대문자와소문자.js
@@ -2,14 +2,9 @@ function solution(my_string) {
     let arr = my_string.split('')
 
     for(let i=0 ; i<arr.length; i++){
-        if(arr[i]=== arr[i].toUpperCase()){
-            arr[i] = arr[i].toLowerCase()
-        }
-        else{
-            arr[i] = arr[i].toUpperCase()
-        }
+       arr[i] === arr[i].toUpperCase() ? arr[i] = arr[i].toLowerCase() : arr[i] = arr[i].toUpperCase()
     }
     return arr.join('')
 }
 
-console.log(solution("ABcDeFGHij"))
+console.log(solution("Park YoungTae"))

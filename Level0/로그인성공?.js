18204f5a3d6e0c95ee9fac66012e391d7ce27e6d
0taepark <ytpark1665@gmail.com>
2022-12-03 20:27:04 +0900

    Level0 로그인성공? 수정

--- Level0/로그인성공?.js
+++ Level0/로그인성공?.js
@@ -1,21 +1,11 @@
 function solution(id_pw, db) {
-    let result
-    const userId = id_pw[0]
-    const userPw = id_pw[1]
+    const userData = db.find(([el])=> el == id_pw[0])
     
-    for (let i=0 ; i<db.length ; i++){
-    if(userId == db[i][0] && userPw == db[i][1])  {
-        result = "login"
+    if(userData){
+       return userData[1] == id_pw[1] ? "login" : "wrong pw"
     }
+    else return "fail"
     
-    else if(userId == db[i][0] && userPw !==db[i][1]){
-        result = "wrong pw"
-    }
-    else if(userId !== db[i][0] && userPw !== db[i][1]){
-        result = "fail"
-    }
-    }
-        return  result
     }
 
     console.log(solution(["meosseugi", "1234"],[["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]]))

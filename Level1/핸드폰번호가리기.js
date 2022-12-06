ccdd6c4182d32ebd2ac7e6f26cf20a6073eefa74
0taepark <ytpark1665@gmail.com>
2022-12-06 16:35:23 +0900

    Level0 핸드폰번호가리기

---
+++ Level1/핸드폰번호가리기.js
@@ -0,0 +1,15 @@
+function solution(phone_number) {
+    let result = ""
+for(let i=0; i<phone_number.length; i++){
+    if(i<phone_number.length-4){
+        result += "*"
+    }
+    else{
+        result += phone_number[i]
+    }
+}
+   return result
+}
+
+console.log(solution("01033334444"))
+console.log(solution("027778888"))

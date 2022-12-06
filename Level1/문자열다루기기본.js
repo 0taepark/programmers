d26920d9f7494925897ded3e0b2629d3c6478ebe
0taepark <ytpark1665@gmail.com>
2022-12-06 17:35:56 +0900

    Level0 문자열다루기기본

---
+++ Level1/문자열다루기기본.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    if (s.length !== 4 && s.length !== 6) {
+      return false;
+    }
+  for (let i=0 ; i<s.length; i++){
+      if(isNaN(s[i]) === true){
+          return false
+      }
+  }
+     return true
+  }
+  
+  console.log(solution("a234"))
+  console.log(solution("1234"))
+  console.log(solution("10e1"))

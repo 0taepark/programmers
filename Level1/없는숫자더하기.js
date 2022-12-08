81b7fc9c31375a0fd78e0438d305c53fda0384c0
0taepark <ytpark1665@gmail.com>
2022-12-08 18:59:39 +0900

    Level1 없는숫자더하기

---
+++ Level1/없는숫자더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    let answer = 0
+    for(let i=0 ; i<=9 ; i++){
+        if(!numbers.includes(i)){
+            answer += i
+        }
+    }
+    return answer
+}
+
+console.log(solution([1,2,3,4,6,7,8,0]))
+console.log(solution([5,8,4,0,6,7,9]))

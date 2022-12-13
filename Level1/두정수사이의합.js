845f720a3d24845b0131dc0c581b48db0e1cc79c
0taepark <ytpark1665@gmail.com>
2022-12-13 20:59:46 +0900

    Level 두정수사이의합

---
+++ Level1/두정수사이의합.js
@@ -0,0 +1,15 @@
+function solution(a, b) {
+    let arr = []
+  if(a == b){
+      return a
+  }
+if(a > b){ 
+  const temp = b
+    b = a
+    a = temp
+ }
+  for(let i = a; i <= b; i++){
+     arr.push(i)
+  }
+    return arr.reduce((a,b) => a + b)
+}
